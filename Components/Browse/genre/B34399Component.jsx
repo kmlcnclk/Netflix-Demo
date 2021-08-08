a0667e9b820b55e233b34f3c23b879fd310cb12e
KamilcanCelik <kamilcancelik5757@gmail.com>
2021-08-08 15:04:05 +0300

    Netflix
    Netflix

--- Components/Browse/genre/B34399Component.jsx
+++ Components/Browse/genre/B34399Component.jsx
@@ -1,7 +1,7 @@
 import { Box } from '@chakra-ui/react';
 import React, { Component } from 'react';
 import Header from '../../Header';
-import MainVideo from '../../MainVideo';
+import MainVideo from '../../Videos/MainVideo';
 
 class B34399Component extends Component {
   render() {
--- Components/Browse/genre/B83Component.jsx
+++ Components/Browse/genre/B83Component.jsx
@@ -1,7 +1,7 @@
 import { Box } from '@chakra-ui/react';
 import React, { Component } from 'react';
 import Header from '../../Header';
-import MainVideo from '../../MainVideo';
+import MainVideo from '../../Videos/MainVideo';
 
 class B83Component extends Component {
   render() {
